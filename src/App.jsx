abe203017eb67cf9ea8e5941a256b796ab4b1ace
Pranai-1 <reddypranai2017@gmail.com>
2024-05-24 16:31:25 +0530

    review page of the college with images

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import HomePage from './Components/Home';
 import ScaleReport from './Components/Report';
+import Demo from './Components/Demo';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='dowellscale' Component={HomePage} />
         <Route path='/scalereport' Component={ScaleReport} />
+        <Route path='/demo' Component={Demo} />
       </Routes>
     </div>
   );
